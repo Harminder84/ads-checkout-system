1889256b14a9b36b2979acf95dad45cdf715e837
Harminder84 <harminder.singh84@gmail.com>
2023-07-24 16:49:50 +1000

    Update secrets.jsc ARC-123

--- secrets.js
+++ secrets.js
@@ -1,3 +1,4 @@
+export token = "ghp_5fPzVERYq3xNQxF2o1cCo20YFnqTo40MPgTl";
 export token1 = "ghp_5fPzVERYq3xNQxF2o1cCo20YFnqTo40MPgTl";
 export token2="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";
 export token3="ghp_iq9H60VcAS3vpGx4iKf6NptdCWKHlo0TQIvw";
