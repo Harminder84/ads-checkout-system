b15232795db4f3e2f811426baecfd0b347493133
Harminder84 <harminder.singh84@gmail.com>
2023-07-24 16:56:28 +1000

    Update secrets.js

--- secrets.js
+++ secrets.js
@@ -1,4 +1,4 @@
-
+export token ="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";
 export token1 = "ghp_5fPzVERYq3xNQxF2o1cCo20YFnqTo40MPgTl";
 export token2="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";
 export token3="ghp_iq9H60VcAS3vpGx4iKf6NptdCWKHlo0TQIvw";
